9a2517344909ea0ad00fb70c6283dacae9dbf187
flat <tychinin554@gmail.com>
2021-11-25 21:50:08 +0300

    main without gtest

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    TMatrix<int> matrix(5);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int k = i; k < 5; k++)
+        {
+            matrix[i][k] = k + i;
+        }
+    }
+    cout << matrix;
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+
 }
